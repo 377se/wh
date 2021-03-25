a453333b15f7be7f27f16bbffdaf1c63aab345d3
Tomas Coox <tomas@joox.se>
2021-03-25 17:16:13 +0100

    add alert-comp from gameday

---
+++ components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div 
+    :class="alertClass" 
+    uk-alert>
+    <div
+      v-for="er in errorlist"
+      :key="er.Name"
+    >
+      {{ er.Value }}
+    </div>
+
+    <div>
+      {{ message }}
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    errorlist:{
+      type: Array,
+      default: () => [],
+      required: true
+    },
+    message:{
+      type: String,
+      default: () => '',
+      required: true
+    },
+    alertClass:{
+      type: String,
+      default: () => 'uk-alert-danger',
+      required: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
