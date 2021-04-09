d45db46d927ad4027cc36d4bd7e0d9a6824cb4bb
Tomas Coox <tomas@joox.se>
2021-04-09 14:58:27 +0200

    add edit-panel to orderinfo

--- pages/order/_id.vue
+++ pages/order/_id.vue
@@ -15,7 +15,6 @@
                 </div>
             </div>
             <div id="sc-page-content">
-
                 <ScCard>
                     <ScCardHeader separator>
                         <div class="uk-flex uk-flex-middle">
@@ -30,71 +29,151 @@
                     </ScCardHeader>
                     <ScCardBody>
                         <div class="uk-flex">
-                            <table :class="{'uk-width-2-3': infoEditorVisible }" class="uk-card uk-box-shadow-small uk-table uk-table-small uk-text-small uk-margin-remove-bottom uk-margin-medium-right">
+                            <table :class="{'uk-width-2-3': adressEditorVisible }" class="uk-card uk-box-shadow-small uk-table uk-table-small uk-text-small uk-margin-remove-bottom">
                                 <tbody>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Kund</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Kund</strong></td>
                                         <td class="border-bottom uk-width-4-5">
-                                            <div>{{ orderInfo.Address.FirstName }} {{ orderInfo.Address.LastName}}</div>
-                                            <div>{{ orderInfo.Address.Address1 }}</div>
-                                            <div>{{ orderInfo.Address.PostalCode }} {{ orderInfo.Address.City}}</div>
-                                            <div>{{ orderInfo.Address.Country }}</div>
-                                            <div>{{ orderInfo.Address.Email }}</div>
-                                            <div>{{ orderInfo.Address.Mobile }}</div>
+                                            <div class="uk-flex uk-flex-between">
+                                                <div>
+                                                    <div>{{ orderInfo.Address.FirstName }} {{ orderInfo.Address.LastName}}</div>
+                                                    <div>{{ orderInfo.Address.Address1 }}</div>
+                                                    <div>{{ orderInfo.Address.PostalCode }} {{ orderInfo.Address.City}}</div>
+                                                    <div>{{ orderInfo.Address.Country }}</div>
+                                                    <div>{{ orderInfo.Address.Email }}</div>
+                                                    <div>{{ orderInfo.Address.Mobile }}</div>
+                                                </div>
+                                                 <div class="editicon" @click="getAdressToEdit(orderInfo.Address.AddressId)"> <!-- EDITERA ADDRESS -->
+                                                    <i class="mdi mdi-file-edit md-color-green-600"></i>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Orderdatum</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Orderdatum</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.OrderDate }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Voucherkod</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Voucherkod</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.Voucher }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Konto-id</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Konto-id</strong></td>
                                         <td v-if="orderInfo.AccountId" class="border-bottom uk-width-4-5">{{ orderInfo.AccountId }}</td>
                                         <td v-else class="border-bottom uk-width-4-5"><span class="uk-badge md-bg-gray-600">Denna order saknar information om provider-Id</span></td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Trackingnummer</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Trackingnummer</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.TrackingNumber }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Betalningsmetod</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Betalningsmetod</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.PaymentMethod }}, {{ orderInfo.Currency }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Leveransdatum</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Leveransdatum</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.ShippingDate }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Följesedel</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Följesedel</strong></td>
                                         <td class="border-bottom uk-width-4-5">{{ orderInfo.HasBeenPrinted ? 'Utskriven' : 'Ej utskriven' }}</td>
                                     </tr>
                                     <tr>
-                                        <td class="border-bottom border-right uk-width-1-5">Status</td>
+                                        <td class="border-bottom border-right uk-width-1-5"><strong>Status</strong></td>
                                         <td class="border-bottom uk-width-4-5">
-                                            <select v-model="orderInfo.StatusId" class="uk-select">
-                                                <option v-for="statusId in orderInfo.StatusList" :key="statusId.Id" :value="statusId.Id">{{ statusId.Name }}</option>
-                                            </select>
+                                            <div class="uk-width-1-1">
+                                                <div class="sc-input-wrapper sc-input-wrapper-outline sc-input-filled">
+                                                <client-only>
+                                                    <Select2
+                                                        id="select-statusList"
+                                                        v-model="orderInfo.StatusId"
+                                                        :options="orderInfo.StatusList.map(({ Id, Name }) => ({ id: Id, text: Name }))"
+                                                        :settings="{ 'width': '100%', 'closeOnSelect': true }"
+                                                        @change="updateOrder()"
+                                                    >
+                                                    </Select2>
+                                                </client-only>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td class="border-right uk-width-1-5">Kommentar</td>
+                                        <td class="border-right uk-width-1-5"><strong>Kommentar</strong></td>
                                         <td class="uk-width-4-5">
-                                            <textarea v-model="orderInfo.Comment" rows="7" cols="80" class="uk-width-1-1 uk-text-small"></textarea>
+                                            <textarea v-model="orderInfo.Comment" rows="7" cols="80" class="uk-width-1-1 uk-text-small" @blur="updateOrder()"></textarea>
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <div v-if="infoEditorVisible" :class="{'uk-width-1-3': infoEditorVisible }" class="uk-card uk-padding-small">
-                               EDITOR FÖR ORDERINFO
+                            <!-- EDIT ADRESS -->
+                            <div v-if="adressEditorVisible" :class="{'uk-width-1-3': adressEditorVisible }" class="uk-card uk-padding-small uk-margin-medium-left">
+                                <div class="uk-flex uk-flex-between">
+                                    <h3 class="uk-card-title uk-padding-remove-vertical uk-padding-remove-horizontal">Editera address</h3>
+                                    <span class="closeicon" @click="adressEditorVisible = false"><i class="mdi mdi-close-circle md-color-grey-600"></i></span>
+                                </div>
+                                <!-- För- och efternamn -->
+                                <div class="uk-margin">
+                                    <div class="uk-flex uk-flex-between">
+                                        <ScInput v-model="adressItem.FirstName" state="fixed" mode="outline" extra-classes="uk-form-small">
+                                            <label>Förnamn</label>
+                                        </ScInput>
+                                        <ScInput v-model="adressItem.LastName" state="fixed" mode="outline" extra-classes="uk-form-small uk-margin-small-left">
+                                            <label>Efternamn</label>
+                                        </ScInput>
+                                    </div>
+                                </div>
+                                <!-- Gatuaddress -->
+                                <div class="uk-margin">
+                                    <ScInput v-model="adressItem.Address1" state="fixed" mode="outline" extra-classes="uk-form-small">
+                                        <label>Gatuaddress</label>
+                                    </ScInput>
+                                </div>
+                               <!-- Postadress -->
+                                <div class="uk-margin">
+                                    <div class="uk-flex uk-flex-between">
+                                        <ScInput v-model="adressItem.PostalCode" state="fixed" mode="outline" extra-classes="uk-form-small">
+                                            <label>Postnummer</label>
+                                        </ScInput>
+                                        <ScInput v-model="adressItem.City" state="fixed" mode="outline" extra-classes="uk-form-small uk-margin-small-left">
+                                            <label>Stad</label>
+                                        </ScInput>
+                                    </div>
+                                </div>
+                                <!-- Land -->
+                                <div class="uk-margin-medium-bottom uk-width-1-1">
+                                    <div class="sc-input-wrapper sc-input-wrapper-outline sc-input-filled">
+                                    <label class="select-label" for="select-countryList">Land</label>
+                                    <client-only>
+                                        <Select2
+                                            id="select-countryList"
+                                            v-model="adressItem.CountryId"
+                                            :options="countries.map(({ CountryId, Name }) => ({ id: CountryId, text: Name }))"
+                                            :settings="{ 'width': '100%', 'closeOnSelect': true }"
+                                        >
+                                        </Select2>
+                                    </client-only>
+                                    </div>
+                                </div>
+                                <!-- Email -->
+                                <div class="uk-margin">
+                                    <ScInput v-model="adressItem.Email" state="fixed" mode="outline" extra-classes="uk-form-small">
+                                        <label>Email</label>
+                                    </ScInput>
+                                </div>
+                                <!-- Mobile -->
+                                <div class="uk-margin">
+                                    <ScInput v-model="adressItem.Mobile" state="fixed" mode="outline" extra-classes="uk-form-small">
+                                        <label>Mobil</label>
+                                    </ScInput>
+                                </div>
+                                <button v-waves.button.light class="sc-button sc-button-primary" @click.prevent="updateAdress()">
+                                    UPPDATERA
+                                </button>
                             </div>
                         </div>
                     </ScCardBody>
                 </ScCard>
-                <ScCard class="uk-margin-large-top">
+                <ScCard class="uk-margin-medium-top">
                     <ScCardHeader separator>
                         <div class="uk-flex uk-flex-middle">
                             <div class="uk-flex-1">
@@ -129,7 +208,7 @@
                                         </td>
                                         <td class="border-bottom border-right uk-text-center">
                                             <div class="wastebasket" @click="deleteItem(orderItemInList.ItemId)"> <!-- TA BORT PRODUKT -->
-												<i class="mdi mdi-delete-forever md-color-red-600"></i>
+												<i class="mdi mdi-delete-forever md-color-red-600 sc-icon-28"></i>
 											</div>
                                         </td>
                                         <td class="border-bottom border-right image-column-width"><img :src="orderItemInList.ImageName"></td>
@@ -381,15 +460,17 @@ export default {
     data () {
 		return {
             errors: null,
-            infoEditorVisible: false,
+            adressEditorVisible: false,
             updateEditorVisible: false,
             addEditorVisible: false,
             orderInfo: {},
             orderContent: {},
             orderItem: null,
+            adressItem: null,
             articleNumber: null,
             articleDetails: null,
             sizeOptionsList: [],
+            countries: [],
         }
     },
     watch: {
@@ -478,6 +559,49 @@ export default {
                 _this.hidePageOverlaySpinner()
 			})
 		},
+        async updateOrder() {
+			let _this = this
+            _this.showPageOverlaySpinner()
+			await this.$axios.$post('/webapi/OrderHandling/PostUpdateOrder', _this.orderInfo )
+			.then(function (response) {
+                try {
+                    if (response.ErrorList != null ) {
+                        _this.hidePageOverlaySpinner()
+                    } else {
+                        _this.orderInfo = response
+                        _this.hidePageOverlaySpinner()
+                    }
+                } catch(err) {
+                    console.log(err)
+                }
+			})
+			.catch(function (error) {
+                console.log(error)
+                _this.hidePageOverlaySpinner()
+			})
+		},
+        async updateAdress() {
+			let _this = this
+            _this.showPageOverlaySpinner()
+			await this.$axios.$post('/webapi/Address/PostUpdateAddress?orderId=' + _this.orderInfo.OrderId, _this.adressItem )
+			.then(function (response) {
+                try {
+                    if (response.ErrorList != null ) {
+                        _this.hidePageOverlaySpinner()
+                    } else {
+                        _this.orderInfo = response
+                        _this.adressEditorVisible = false
+                        _this.hidePageOverlaySpinner()
+                    }
+                } catch(err) {
+                    console.log(err)
+                }
+			})
+			.catch(function (error) {
+                console.log(error)
+                _this.hidePageOverlaySpinner()
+			})
+		},
         async getItemToEdit(itemId) {
 			let _this = this
             _this.showPageOverlaySpinner()
@@ -493,6 +617,28 @@ export default {
                         _this.addEditorVisible = false
                         _this.hidePageOverlaySpinner()
                         // _this.$store.commit('setAlertHidden', 4) Vi behöver alert här
+                    }
+                } catch(err) {
+                    console.log(err)
+                }
+			})
+			.catch(function (error) {
+                console.log(error)
+                _this.hidePageOverlaySpinner()
+			})
+		},
+        async getAdressToEdit(adressId) {
+			let _this = this
+            _this.showPageOverlaySpinner()
+			await this.$axios.$get('/webapi/Address/GetAddress?addressId=' + adressId)
+			.then(function (response) {
+                try {
+                    if (response.ErrorList != null ) {
+                        _this.hidePageOverlaySpinner()
+                    } else {
+                        _this.adressItem = response
+                        _this.adressEditorVisible = true
+                        _this.hidePageOverlaySpinner()
                     }
                 } catch(err) {
                     console.log(err)
@@ -538,12 +684,14 @@ export default {
     async fetch () {
         try {
             this.showPageOverlaySpinner()
-            const [ orderInfo, orderContent ] = await Promise.all([
+            const [ orderInfo, orderContent, countries ] = await Promise.all([
                 this.$axios.$get('/webapi/Order/GetOrder?orderId=' + this.$route.params.id),
                 this.$axios.$get('/webapi/Order/GetOrderContent?orderId=' + this.$route.params.id),
+                this.$axios.$get('/webapi/Address/GetCountries'),
             ])
             this.orderInfo = orderInfo
             this.orderContent = orderContent
+            this.countries = countries
             this.hidePageOverlaySpinner()
         } catch (err) {
             console.log(err);
