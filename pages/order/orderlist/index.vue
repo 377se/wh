ca21faba638cf2e5a2a1a11cdaba249fd1456e79
Tomas Coox <tomas@joox.se>
2021-04-22 10:26:33 +0200

    add shop-menu, implement delivered and delayed

--- pages/order/orderlist/index.vue
+++ pages/order/orderlist/index.vue
@@ -17,18 +17,59 @@
             <div id="sc-page-content">
                 <ScCard>
                     <ScCardBody>
-                        <div class="uk-margin-medium-bottom uk-padding-small actionpanel" uk-sticky="offset: 60">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab labore laboriosam? Eveniet ducimus perferendis voluptatum unde, dolor qui ullam consequatur, fugiat adipisci tempore numquam fuga distinctio repudiandae repellat itaque autem in illo? Perferendis saepe recusandae, omnis, quam ut quaerat eum vero, nihil dolorum laborum nulla! Iure provident neque in est, inventore eum eligendi dignissimos perspiciatis accusamus deleniti nihil maiores labore illum possimus iste hic delectus, explicabo vel molestiae temporibus excepturi aliquid blanditiis rem. Ab, libero. Voluptate sint, adipisci est maxime eveniet officiis eius porro, alias deserunt itaque ut, esse illum? Iste, dolore qui. Asperiores inventore fugit quod labore voluptatibus.
+                        <div class="uk-margin-medium-bottom uk-padding-small uk-padding-remove-horizontal actionpanel" uk-sticky="offset: 60">
+                                <div class="uk-margin-medium-bottom uk-width-1-1">
+                                    <div class="sc-input-wrapper sc-input-wrapper-outline sc-input-filled">
+                                    <!-- <label class="select-label" for="select-shopOptionsList">Shop</label> -->
+                                    <client-only>
+                                        <Select2
+                                            id="select-shopOptionsList"
+                                            v-model="shopId"
+                                            :options="shopOptionsList"
+                                            :settings="{ 'width': '100%', 'placeholder': 'Välj shop för att få ut orderlista', 'closeOnSelect': true }"
+                                            @select="getOrderList"
+                                        >
+                                        </Select2>
+                                    </client-only>
+                                    </div>
+                                </div>
+                            <div v-if="shopId" class="uk-grid-small uk-flex-between" uk-grid>
+                                <div>
+                                    <button v-waves.button.light class="sc-button sc-button-primary" @click.prevent="setOrderAsDelivered()">
+                                        SÄTT SOM LEVERERAD
+                                    </button>
+                                </div>
+                                <div>
+                                    <button v-waves.button.light class="sc-button sc-button-primary" @click.prevent="sendOrderDelayed()">
+                                        FÖRSENAD ORDER
+                                    </button>
+                                </div>
+                                <div class="sc-button sc-button-disabled">
+                                        Markerade ordrar: {{ numberOfSelected }}
+					            </div>
+                            </div>
+                            <Alert
+                                :errorlist="this.errors ? this.errors : []"
+                                :message="this.message ? this.message : ''"
+                                :alertClass="'uk-alert-danger'"
+                                id=1
+                            />
+                            <Alert
+                                :errorlist="this.errors ? this.errors : []"
+                                :message="this.message ? this.message : ''"
+                                :alertClass="'uk-alert-success'"
+                                id=2
+                            />
                         </div>
-                        <div class="uk-overflow-auto">
-                            <table v-for="(order, index) in orderList" :key="order.OrderId" class="uk-table uk-table-small uk-text-small uk-margin-remove">
+                        <div v-if="shopId" class="uk-overflow-auto">
+                            <table v-for="(order, index) in orderList" :key="order.OrderId" class="uk-table uk-table-small uk-text-small uk-margin-remove orderlist">
                                 <thead :class="{ 'hide': index !== 0 }">
                                     <tr class="uk-padding-remove-bottom">
                                         <th class="border-top border-left" style="width: 15px;"></th>
                                         <th class="border-top border-left" style="width: 15px;"></th>
                                         <th class="border-top border-left" style="width: 15px;"></th>
                                         <th class="border-top border-left" style="width: 15px;"></th>
-                                        <th class="border-top border-left uk-text-small" style="text-align: left;">OrderId</th>
+                                        <th class="border-top border-left uk-text-small" style="text-align: left; width: 35px;">OrderId</th>
                                         <th class="border-top border-left uk-text-small" style="text-align: left; width: 150px;">Orderdatum</th>
                                         <th class="border-top border-left uk-text-small" style="text-align: right; width: 70px;">Summa</th>
                                         <th class="border-top border-left uk-text-small" style="width: 70px;">Valuta</th>
@@ -39,11 +80,11 @@
                                 </thead>
                                 <tbody>
                                     <tr class="uk-table-middle">
-                                        <td class="border-top border-left" style="width: 15px;"><input type="checkbox"></td>
+                                        <td class="border-top border-left" style="width: 15px;"><input type="checkbox" v-model="order.IsSelected"></td>
                                         <td class="border-top border-left" style="width: 15px;"><span v-if="order.HasBeenPrinted"><i class="mdi mdi-printer sc-icon-18"></i></span></td>
                                         <td class="border-top border-left" style="width: 15px;"><span v-if="order.ContainsPrintedItem"><i class="mdi mdi-tshirt-crew sc-icon-18"></i></span></td>
                                         <td class="border-top border-left" style="width: 15px;"><span v-if="order.ContainsPreorder"><i class="mdi mdi-pause-circle sc-icon-18"></i></span></td>
-                                        <td class="border-top border-left" style="text-align: left;"><Nuxt-Link :to="order.UrlOrder">{{ order.OrderId }}</Nuxt-Link></td>
+                                        <td class="border-top border-left" style="text-align: left; width: 35px;"><Nuxt-Link :to="order.UrlOrder">{{ order.OrderId }}</Nuxt-Link></td>
                                         <td class="border-top border-left" style="text-align: left; width: 150px;">{{ order.OrderDate }}</td>
                                         <td class="border-top border-left" style="text-align: right; width: 70px;">{{ order.Ordersum }}</td>
                                         <td class="border-top border-left" style="width: 70px;">{{ order.Currency }}</td>
@@ -72,36 +113,83 @@
 </template>
 
 <script>
+import Alert from '~/components/Alert'
 
 export default {
+	components: {
+		Alert,
+		Select2: process.client ? () => import('~/components/Select2') : null,
+    },
     data () {
 		return {
-            orderList: null,
+            orderList: [],
+            shopId: null,
+            shopOptionsList: [],
             errors: null,
+            message: '',
         }
     },
     watch: {
-
     },
 	mounted: function () {
-
     },
     computed: {
-
+        numberOfSelected: function () {
+            let counter = 0;
+            for (const order of this.orderList) {
+                if (order.IsSelected === true) counter++;
+            }
+            return counter
+        }
     },
     methods: {
-        async getMethod() {
+        resetIsSelected () {
+            for (const order of this.orderList) {
+                if (order.IsSelected === true) order.IsSelected = false
+            }
+        },
+        async setOrderAsDelivered() {
 			let _this = this
             _this.showPageOverlaySpinner()
-			await this.$axios.$get('/webapi/Address/GetCountries')
+			await this.$axios.$post('/webapi/OrderHandling/PostSetOrderAsDelivered', _this.orderList)
 			.then(function (response) {
                 try {
                     if (response.ErrorList != null ) {
+                        _this.errors = response.ErrorList
                         _this.hidePageOverlaySpinner()
-                        _this.$store.commit('setAlertVisible', 4)
+                        _this.resetIsSelected()
+                        _this.$store.commit('setAlertVisible', 1)
                     } else {
+                        _this.message = response.Message
                         _this.hidePageOverlaySpinner()
-                        _this.$store.commit('setAlertHidden', 4)
+                        _this.resetIsSelected()
+                        _this.$store.commit('setAlertVisible', 2)
+                    }
+                } catch(err) {
+                    console.log(err)
+                }
+			})
+			.catch(function (error) {
+                console.log(error)
+                _this.hidePageOverlaySpinner()
+			})
+		},
+        async sendOrderDelayed() {
+			let _this = this
+            _this.showPageOverlaySpinner()
+			await this.$axios.$post('/webapi/OrderHandling/PostSendOrderDelayed', _this.orderList)
+			.then(function (response) {
+                try {
+                    if (response.ErrorList != null ) {
+                        _this.errors = response.ErrorList
+                        _this.hidePageOverlaySpinner()
+                        _this.resetIsSelected()
+                        _this.$store.commit('setAlertVisible', 1)
+                    } else {
+                        _this.message = response.Message
+                        _this.hidePageOverlaySpinner()
+                        _this.resetIsSelected()
+                        _this.$store.commit('setAlertVisible', 2)
                     }
                 } catch(err) {
                     console.log(err)
@@ -120,13 +208,27 @@ export default {
             this.$store.commit('toggleProgressOverlay', true);
             this.$store.commit('togglePageOverlay', true)
         },
+        async getOrderList() {
+			let _this = this
+            _this.showPageOverlaySpinner()
+			await this.$axios.$get('/webapi/Order/GetOrderlist?shopId=' + _this.shopId +'&orderdate=2021-04-14&printStatus=0&hasPrint=0&preorderStatus=2&backorder=0&comment=0&sortorder=desc&pageNum=1')
+			.then(function (orderlist) {
+                _this.orderList = orderlist
+                _this.resetIsSelected()
+                _this.hidePageOverlaySpinner()
+			})
+			.catch(function (error) {
+                console.log(error)
+                _this.hidePageOverlaySpinner()
+			})
+		},
     },
     async fetch () {
         try {
-            const [ orderList ] = await Promise.all([
-                this.$axios.$get('/webapi/Order/GetOrderlist?shopId=2&orderdate=2021-04-14&printStatus=0&hasPrint=0&preorderStatus=2&backorder=0&comment=0&sortorder=desc&pageNum=1'),
+            const [ shops ] = await Promise.all([
+                this.$axios.$get('/webapi/Shop/GetShopList'),
             ])
-            this.orderList = orderList
+            this.shopOptionsList = shops.map(({ ShopId, ShopName }) => ({ id: ShopId, text: ShopName }))
         } catch (err) {
             console.log(err);
         }
@@ -155,5 +257,8 @@ export default {
     }
     .actionpanel {
         background-color: #fff;
+    }
+    .orderlist {
+        table-layout: fixed;
     }
 </style>
