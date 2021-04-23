fe716819494c7239827250ebc336e055fc54425f
Tomas Coox <tomas@joox.se>
2021-04-23 09:36:34 +0200

    add back-button on single deliverynote

--- pages/deliverynote/_id.vue
+++ pages/deliverynote/_id.vue
@@ -1,5 +1,12 @@
 <template>
-    <Deliverynotes :orders="this.$route.params.id" :isUnifaunTrue="false" />
+    <div id="sc-page-wrapper">
+      <div id="sc-page-content">
+        <button v-waves.button.light class="sc-button sc-button-primary uk-margin-medium-right" @click="$router.go(-1)">
+            TILLBAKA
+        </button>
+        <Deliverynotes :orders="this.$route.params.id.split(',')" :isUnifaunTrue="false" />
+      </div>
+    </div>
 </template>
 
 <script>
