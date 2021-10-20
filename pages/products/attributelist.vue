355fe20fc8e9c22d26af831893dcec15d90e58ea
Henrik Stridsman <henrik@377.se>
2021-10-20 14:19:47 +0200

    Attributelist started

---
+++ pages/products/attributelist.vue
@@ -0,0 +1,156 @@
+<template>
+    <div v-if="$fetchState.pending">
+        <div id="sc-page-wrapper">
+        </div>
+    </div>
+    <div v-else>
+        <div id="sc-page-wrapper">
+            <div id="sc-page-top-bar" class="sc-top-bar">
+                <div class="sc-top-bar-content sc-padding-medium-top sc-padding-medium-bottom uk-flex-1">
+                    <div class="uk-flex-1">
+                        <h1 class="sc-top-bar-title uk-display-inline">Attributlista</h1>
+                    </div>
+                </div>
+            </div>
+            <div id="sc-page-content">
+                <ScCard :key="render">
+                    <ScCardBody>
+                        <div class="uk-margin-medium-bottom uk-padding-small uk-padding-remove-horizontal actionpanel" uk-sticky="offset: 45">
+                            <div class="uk-margin-medium-bottom uk-width-1-1">
+                              <div class="sc-input-wrapper sc-input-wrapper-outline sc-input-filled">
+                              <client-only>
+                                <Select2
+                                    id="select-shopOptionsList"
+                                    v-model="shopId"
+                                    :options="shopOptionsList"
+                                    :settings="{ 'width': '100%', 'placeholder': 'Välj shop', 'closeOnSelect': true }"
+                                >
+                                </Select2>
+                              </client-only>
+                              </div>
+                            </div>
+                            <div class="uk-margin-medium-bottom uk-width-1-1">
+                              <div class="sc-input-wrapper sc-input-wrapper-outline sc-input-filled">
+                              <client-only>
+                                <Select2
+                                    id="select-producttypes"
+                                    v-model="productType"
+                                    :options="producttypes"
+                                    :settings="{ 'width': '100%', 'placeholder': 'Välj produkttyp', 'closeOnSelect': true }"
+                                >
+                                </Select2>
+                              </client-only>
+                              </div>
+                            </div>
+                            <div class="uk-flex uk-flex-around uk-margin-medium-bottom">
+                              <button v-waves.button.light class="sc-button sc-button-primary" @click.prevent="getAttributeList()">Hämta
+                              </button>
+                            </div>
+                        </div>
+                    </ScCardBody>
+                </ScCard>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import Alert from '~/components/Alert'
+
+export default {
+	components: {
+		Alert,
+		Select2: process.client ? () => import('~/components/Select2') : null
+    },
+    data () {
+		return {
+          attributeList: [],
+          producttypes: [],
+          shopId: null,
+          productType: null,
+          errors: null,
+          message: '',
+          render: false,
+        }
+    },
+    watch: {
+    },
+    mounted() {
+        if (!this.loaded) this.$fetch()
+    },
+    methods: {
+    async getAttributeList() {
+			let _this = this
+      _this.$store.dispatch('setBusyOn')
+        await this.$axios.$get('/webapi/Attribute/GetArticleListByProductType?shopId=' + _this.shopId +'&productTypeId' + _this.productType)
+        .then(function (attributeList) {
+          _this.attributeList = attributeList
+          _this.$store.dispatch('setBusyOff')
+        }
+      )
+      .catch(function (error) {
+        console.log(error)
+        _this.$store.dispatch('setBusyOff')
+      })
+		},
+    },
+    async fetch () {
+        this.$store.dispatch('setBusyOn')
+        try {
+            const [ shops, producttypes ] = await Promise.all([
+                this.$axios.$get('/webapi/Shop/GetShopList'),
+                this.$axios.$get('/webapi/Attribute/GetAttributeListForProductTypes')
+            ])
+            this.producttypes = producttypes.map(({ Id, Name }) => ({ id: Id, text: Name }))
+            this.shopOptionsList = shops.map(({ ShopId, ShopName }) => ({ id: ShopId, text: ShopName }))
+            if (this.$route.query.shopid || this.$route.query.producttype) {
+                if (this.$route.query.shopid) {
+                    this.shopId = this.$route.query.shopid
+                }
+                if (this.$route.query.producttype) {
+                    this.productType = this.$route.query.producttype
+                }
+                this.getAttributeList()
+            }
+        this.$store.dispatch('setBusyOff')
+        } catch (err) {
+            console.log(err)
+            this.$store.dispatch('setBusyOff')
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .border-all {
+        border: 1px solid #ccc;
+    }
+    .border-left {
+        border-left: 1px solid #ccc;
+    }
+    .border-right {
+        border-right: 1px solid #ccc;
+    }
+    .border-top {
+        border-top: 1px solid #ccc;
+    }
+    .border-bottom {
+        border-bottom: 1px solid #ccc;
+    }
+    .hide {
+        display: none;
+    }
+    .actionpanel {
+        background-color: #fff;
+    }
+    .orderlist {
+        table-layout: fixed;
+    }
+    .sticky-down-right {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        z-index: 1;
+    }
+</style>
