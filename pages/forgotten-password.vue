3c1971d5d68b5bf4fba925bc86d741465ab9286b
Tomas Coox <tomas@joox.se>
2021-11-02 19:48:09 +0100

    forgotten password initpage

---
+++ pages/forgotten-password.vue
@@ -0,0 +1,94 @@
+<template>
+    <div v-if="$fetchState.pending">
+        <div id="sc-page-wrapper">
+        </div>
+    </div>
+    <div v-else>
+        <div id="sc-page-wrapper">
+            <div id="sc-page-top-bar" class="sc-top-bar">
+                <div class="sc-top-bar-content sc-padding-medium-top sc-padding-medium-bottom uk-flex-1">
+                    <div class="uk-flex-1">
+					    <h1 class="sc-top-bar-title uk-display-inline">Glömt lösenordet?</h1>
+				    </div>
+                </div>
+            </div>
+            <div id="sc-page-content">
+                <ScCard>
+                    <ScCardBody>
+                        <Alert
+                            :errorlist="errors"
+                            message=""
+                            :alertClass="'uk-alert-danger'"
+                            id=1
+                        />
+                        <!-- Email -->
+                        <div class="uk-margin-medium-top uk-margin-medium-bottom">
+                            <ScInput v-model="email" state="fixed" mode="outline" extra-classes="uk-form-small" placeholder="Skriv in din email">
+                                <label>Email</label>
+                            </ScInput>
+                        </div>
+                        <button v-waves.button.light class="sc-button sc-button-primary uk-margin-small-top" @click="getResetLink">
+                            SKICKA ÅTERSTÄLLNINGSINFORMATION
+                        </button>
+                    </ScCardBody>
+                </ScCard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ScInput from '~/components/Input'
+    import Alert from '~/components/Alert'
+
+    export default {
+        components: {
+            Alert,
+            ScInput,
+        },
+        data() {
+            return {
+                errors: null,
+                email: null,
+            }
+        },
+        methods: {
+            async getResetLink() {
+                let _this = this
+                _this.$store.commit('setAlertHidden', 1)
+                _this.$store.dispatch('setBusyOn')
+                await this.$axios.$post('/webapi/Account/GetResetLink?email=' + _this.email)
+                .then(function (resetinfo) {
+                    if (resetinfo.ErrorList != null) {
+                        _this.errors = resetinfo.ErrorList
+                        _this.$store.commit('setAlertVisible', 1)
+                        _this.$store.dispatch('setBusyOff')
+                    } else {
+                        _this.$store.dispatch('setBusyOff')
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                    _this.$store.dispatch('setBusyOff')
+                })
+            },
+        },
+        async fetch () {
+            this.$store.dispatch('setBusyOn')
+            try {
+                const [ emptyresetobject ] = await Promise.all([
+                    this.$axios.$get('/webapi/Account/GetResetLink?email=' + this.guid),
+                ])
+                this.emptyResetObject = emptyresetobject
+                this.$store.dispatch('setBusyOff')
+            } catch (err) {
+                console.log(err);
+                this.$store.dispatch('setBusyOff')
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
